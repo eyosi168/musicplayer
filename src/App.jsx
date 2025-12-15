9196422099cd2cf3bd12d2fb2fc823aa685d2d62
eyosi168 <eyosisol123@gmail.com>
2025-12-15 17:26:20 +0300

    sencond one

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import MusicPlayer from "./components/MusicPlayer";
+import AllSongs from "./components/AllSongs";
+import Playlists from "./components/Playlists";
 
 function App() {
-
-
   return (
     <>
-      
+      <BrowserRouter>
+        <div className="app">
+          <main className="app-main">
+            <div className="player-section">
+              <MusicPlayer></MusicPlayer>
+            </div>
+            <div className="content-section">
+              <Routes>
+                <Route path="/" element={<AllSongs />} />
+                <Route path="/playlists" element={<Playlists />} />
+              </Routes>
+            </div>
+          </main>
+        </div>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AllSongs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AllSongs() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AllSongs
